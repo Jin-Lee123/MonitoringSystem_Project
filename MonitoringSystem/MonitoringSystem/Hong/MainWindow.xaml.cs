1f575d8348f7b0cccbd5f8ede55d997eb702a739
홍건의 <keaam12@google.com>
2021-09-01 17:41:21 +0900

    복구1

--- MonitoringSystem/MonitoringSystem/Hong/MainWindow.xaml.cs
+++ MonitoringSystem/MonitoringSystem/Hong/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace MonitoringSystem
 {
@@ -23,6 +24,58 @@ namespace MonitoringSystem
         public MainWindow()
         {
             InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Tick += new EventHandler(UpdateTimer_Tick);
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Start();
+        }
+
+        private void UpdateTimer_Tick(object sender, EventArgs e)
+        {
+            DisplayDateTextBlock.Text = DateTime.Now.ToString(@"HH:mm");
+        }
+
+        private void ListViewItem_MouseEnter(object sender, MouseEventArgs e)
+        {
+            // Set tooltip visibility
+
+            if (Tg_Btn.IsChecked == true)
+            {
+                tt_home.Visibility = Visibility.Collapsed;
+                tt_contacts.Visibility = Visibility.Collapsed;
+                tt_messages.Visibility = Visibility.Collapsed;
+                tt_maps.Visibility = Visibility.Collapsed;
+                tt_settings.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                tt_home.Visibility = Visibility.Visible;
+                tt_contacts.Visibility = Visibility.Visible;
+                tt_messages.Visibility = Visibility.Visible;
+                tt_maps.Visibility = Visibility.Visible;
+                tt_settings.Visibility = Visibility.Visible;
+            }
+        }
+
+        /*        private void Tg_Btn_Unchecked(object sender, RoutedEventArgs e)
+                {
+                    img_bg.Opacity = 1;
+                }
+
+                private void Tg_Btn_Checked(object sender, RoutedEventArgs e)
+                {
+                    img_bg.Opacity = 0.3;
+                }
+        */
+        private void BG_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Tg_Btn.IsChecked = false;
+        }
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
+
