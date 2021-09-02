13400381ca0fc24cbc9aef42580caee9bcd67d42
jjgweb951101@gmail.com <jone1542%%>
2021-09-02 15:52:54 +0900

    ViewModels 추가

---
+++ MonitoringSystem/MonitoringSystem/ViewModels/RelayCommand.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MonitoringSystem
+{
+    public class RelayCommand<T> : ICommand
+    {
+
+        #region ###대리자 선언###
+        // View에서 넘어온 Command를 실행하는 대리자
+        private readonly Action<T> execute;
+        // View에서 넘어온 Command를 실행할 수 있는 지 확인하는 대리자
+        private readonly Predicate<T> canExecute;
+        #endregion
+
+        #region ###ICommand 필수 내용###
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+        // 명령을 실행할 수 있는지 확인
+        public bool CanExecute(object parameter)
+        {
+            return canExecute?.Invoke((T)parameter) ?? true;
+        }
+        // 명령을 실행
+        public void Execute(object parameter)
+        {
+            execute.Invoke((T)parameter);
+        }
+        #endregion
+
+        #region ###생성자 생성###
+        public RelayCommand(Action<T> execute, Predicate<T> canExecute = null)
+        {
+            //RelayCommand 실행명령은 null일 수 없다.
+            this.execute = execute ?? throw new ArgumentException(nameof(execute));
+            this.canExecute = canExecute;
+        }
+
+        public RelayCommand(Action<T> execute) : this(execute, null)
+        {
+            this.execute = execute;
+        }
+
+        #endregion
+    }
+}
---
+++ MonitoringSystem/MonitoringSystem/ViewModels/TankViews.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MonitoringSystem.ViewModels
+{
+    public class TankViews
+    {
+        #region ###변수 선언###
+
+        #endregion
+
+        #region ###Property 선언###
+
+        #endregion
+
+        #region ###Command 처리
+
+        ICommand clickCommand;
+        public ICommand ClickCommand => clickCommand ?? (clickCommand = new RelayCommand<object>(o => Click(), o => IsClick()));
+
+
+        private bool IsClick()
+        {
+            return true; // Validation Check를 쉽고 간단하게 
+        }
+        private void Click()
+        {
+            try
+            {
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"예외발생 : {ex.Message}");
+            }
+        }
+        #endregion
+    }
+}
